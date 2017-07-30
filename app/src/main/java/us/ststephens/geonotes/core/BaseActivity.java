8b02bb64f114c3f6a4ea9ef01bff271f994f3808
Scott Stephens <stevo4932@gmail.com>
2017-07-29 21:10:56 -0500

    adding home button action

--- app/src/main/java/us/ststephens/geonotes/core/BaseActivity.java
+++ app/src/main/java/us/ststephens/geonotes/core/BaseActivity.java
@@ -4,10 +4,21 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NavUtils;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class BaseActivity extends AppCompatActivity {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     public void requestPermission(String permission, int reqCode) {
         if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{permission}, reqCode);
